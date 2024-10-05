edbc72044e04f61d571a3b66f558e1c960b720c1
Youssef Elkomy <youssefelkomy500@gmail.com>
2024-10-06 01:57:30 +0300

    Generic Repository

---
+++ Linkdev.Talabat.Core.Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linkdev.Talabat.Core.Domain.Contracts
+{
+    public interface IGenericRepository<TEntity, TKey> 
+        where TEntity : BaseEntity<TKey> 
+        where TKey : IEquatable<TKey>   
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync(bool withAsNoTracking);
+
+        Task<TEntity?> GetAsync(int id);
+
+        Task AddAsync(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+
+    }
+}
---
+++ Linkdev.Talabat.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Linkdev.Talabat.Core.Domain.Contracts;
+using Linkdev.Talabat.Persistence.Data;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace Linkdev.Talabat.Persistence.Repositories
+{
+    public class GenericRepository<TEntity, TKey>(StoreContext context) : IGenericRepository<TEntity, TKey>
+        where TEntity : BaseEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool withAsNoTracking)
+            => await (withAsNoTracking ? context.Set<TEntity>().AsNoTracking().ToListAsync() :  context.Set<TEntity>().AsNoTracking().ToListAsync());
+
+        public async Task<TEntity?> GetAsync(int id)
+            => await context.Set<TEntity>().FindAsync(id);
+
+        public async Task AddAsync(TEntity entity)
+            => await context.Set<TEntity>().AddAsync(entity);
+
+        public void Update(TEntity entity)
+            => context.Set<TEntity>().Update(entity);   
+
+        public void Delete(TEntity entity)
+            => context.Set<TEntity>().Remove(entity);
+    }
+}
