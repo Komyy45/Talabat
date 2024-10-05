45edf3911441dc584436683bebb0c3bd9cb3fa9e
Youssef Elkomy <youssefelkomy500@gmail.com>
2024-10-05 16:49:33 +0300

    Onion Architecture

---
+++ Linkdev.Talabat.Dashboard/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Linkdev.Talabat.Dashboard.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Linkdev.Talabat.Dashboard.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
