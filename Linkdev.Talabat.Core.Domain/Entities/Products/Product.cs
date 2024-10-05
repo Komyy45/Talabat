67c6ee395c70a567694ae45d46e366367c958214
Youssef Elkomy <youssefelkomy500@gmail.com>
2024-10-05 18:03:20 +0300

    Product Module - Entities

---
+++ Linkdev.Talabat.Core.Domain/Entities/Products/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Linkdev.Talabat.Core.Domain.Common;
+
+namespace Linkdev.Talabat.Core.Domain.Entities.Products
+{
+    public class Product : BaseEntity<int>
+    {
+        public required string Name { get; set; }
+
+        public required string Description { get; set; }
+
+        public string? PictureUrl { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int? BrandId { get; set; } // Foriegn Key ---> ProuctBrand Entity
+
+        public virtual ProductBrand? Brand { get; set; }
+
+        public int? CategoryId { get; set; } // Foriegn Key ---> ProductCategory Entity
+
+        public virtual ProductCategory? Category { get; set; }
+    }
+}
---
+++ Linkdev.Talabat.Core.Domain/Entities/Products/ProductBrand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Linkdev.Talabat.Core.Domain.Common;
+
+namespace Linkdev.Talabat.Core.Domain.Entities.Products
+{
+    public class ProductBrand : BaseEntity<int>
+    {
+        public required string Name { get; set; }
+    }
+}
