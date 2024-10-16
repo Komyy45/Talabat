3121836b30b53b39a31f61f009a44933255c9b1b
Youssef Elkomy <youssefelkomy500@gmail.com>
2024-10-16 14:16:42 +0300

    Basket Module Entities

---
+++ Linkdev.Talabat.Core.Domain/Entities/Basket/BasketItem.cs
@@ -0,0 +1,19 @@
+﻿namespace Linkdev.Talabat.Core.Domain.Entities.Basket
+{
+    public class BasketItem
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string? PictureUrl { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string? Brand { get; set; }
+
+        public string? Category { get; set; }
+    }
+}
