959b44eff2159c766fa25a8df98779379b68c549
Youssef Elkomy <youssefelkomy500@gmail.com>
2024-11-23 17:08:02 +0200

    OrdersModule - Configuration Classes

---
+++ Linkdev.Talabat.Persistence/Data/Config/Orders/DeliveryMethodConfigurations.cs
@@ -0,0 +1,20 @@
+﻿	using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Linkdev.Talabat.Core.Domain.Entities.Orders;
+
+namespace Linkdev.Talabat.Persistence.Data.Config.Orders
+{
+	internal class DeliveryMethodConfigurations : BaseEntityConfigurations<DeliveryMethod, int>
+	{
+		public override void Configure(EntityTypeBuilder<DeliveryMethod> builder)
+		{
+			base.Configure(builder);
+
+			builder.Property(DM => DM.Cost)
+				   .HasColumnType("decimal(8,2)");
+		}
+	}
+}
---
+++ Linkdev.Talabat.Persistence/Data/Config/Orders/OrderItemConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Linkdev.Talabat.Core.Domain.Entities.Orders;
+
+namespace Linkdev.Talabat.Persistence.Data.Config.Orders
+{
+	internal class OrderItemConfigurations : BaseAuditableEntityConfigurations<OrderItem, int>
+	{
+		public override void Configure(EntityTypeBuilder<OrderItem> builder)
+		{
+			base.Configure(builder);
+
+			builder.OwnsOne(OI => OI.Product);
+
+			builder.Property(OI => OI.Price)
+				   .HasColumnType("decimal(8,2)");
+		}
+	}
+}
